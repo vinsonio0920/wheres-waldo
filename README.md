9e978e9bee934972ca54cd1b465e007c9ba7d426
Vinson <vinsonhe2@gmail.com>
2026-05-05 13:58:42 -0700

    chore: set up project environment
    - Scaffold React app with Vite
    - Install Prettier code formatter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wheres-waldo
+
 A photo tagging app where users are tasked to find a hidden object in a picture.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Let's goooo!</p>
+    </>
+  );
+}
+
+export default App;
