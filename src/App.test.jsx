96406a746a40247dbc530efd48d9bd2c9397f91c
Vinson <vinsonhe2@gmail.com>
2026-05-05 19:03:44 -0700

    chore: set up Vitest to test React

---
+++ src/App.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it } from "vitest";
+
+import App from "./App";
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App title="React" />);
+
+    screen.debug();
+
+    // check if App components renders headline
+  });
+});
