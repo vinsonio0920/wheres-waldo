bf0ac8b883d87118329296930d6b0ed82e011675
Vinson <vinsonhe2@gmail.com>
2026-05-08 12:25:18 -0700

    feat(header): add menu dropdown JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { Link, Outlet } from "react-router-dom";
 import { logoSvg } from "./assets";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleMenuClick = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <>
       <header className={styles.header}>
@@ -12,9 +19,26 @@ function App() {
             Sniper
           </h1>
         </Link>
-        <div className={styles.rightLinks}>
-          <Link to="/attributions">Attributions</Link>
-          <Link to="/">Missions</Link>
+        <div>
+          <button
+            type="button"
+            className={styles.menuButton}
+            aria-label="Menu"
+            onClick={handleMenuClick}
+          >
+            <span className="material-symbols-outlined">menu</span>
+          </button>
+          <ul
+            className={`${styles.rightLinks} ${showDropdown && styles.opened}`}
+            aria-label="Menu dropdown"
+          >
+            <li>
+              <Link to="/attributions">Attributions</Link>
+            </li>
+            <li>
+              <Link to="/">Missions</Link>
+            </li>
+          </ul>
         </div>
       </header>
       <main>
