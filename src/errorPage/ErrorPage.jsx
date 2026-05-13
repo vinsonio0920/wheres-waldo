12dac15d6749ea5e5c9a3e008e3baa0d6a0d9044
Vinson <vinsonhe2@gmail.com>
2026-05-13 13:21:30 -0700

    style(error): add error page styling

--- src/errorPage/ErrorPage.jsx
+++ src/errorPage/ErrorPage.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
 import { logoSvg } from "../assets";
+import styles from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
   return (
-    <>
-      <Link to="/">
+    <div className={styles.errorContainer}>
+      <Link to="/" className={styles.logoLink}>
         <img src={logoSvg} width="32" alt="Logo" />
-        Sniper
+        <span>Sniper</span>
       </Link>
       <h1>Wrong target!</h1>
-      <p>
-        The page you are looking for doesn't exist!{" "}
-        <Link to="/">Head back to the homepage.</Link>
-      </p>
-    </>
+      <p>The page you are looking for doesn't exist!</p>
+      <Link to="/" className={styles.homepageLink}>
+        Head back to the homepage.
+      </Link>
+    </div>
   );
 };
 
