c9acac3ed5db9a31474156e111cc31ca4e22370d
Vinson <vinsonhe2@gmail.com>
2026-05-14 11:13:56 -0700

    feat(attributions): add attributions JSX

--- src/attributions/Attributions.jsx
+++ src/attributions/Attributions.jsx
@@ -1,6 +1,49 @@
 const Attributions = () => {
   return (
-    <p>Attributions! After this we will be done with the frontend for now!</p>
+    <>
+      <h1>Attributions</h1>
+      <p data-testid="attributions-para">
+        All of the photos in this website was taken from the internet. I am
+        currently not skilled enough (yet!) to make all these amazing photos!
+        Besides that, not much else to say. I hope you enjoyed the website. The
+        majority of this website's design was based on Where's Waldo? Play
+        Online which I highly recommend. I've spent a good chunk of time playing
+        all the levels in the website and had a lot of fun doing so. Now.. is
+        the word count there yet?
+      </p>
+      <ul aria-label="Attributions list">
+        <li>
+          Snipe the gymnasts photo by u/outdoorschillguy on{" "}
+          <a
+            target="_blank"
+            href="https://www.reddit.com/r/FindTheSniper/top"
+            rel="noopener noreferrer"
+          >
+            Find the Sniper
+          </a>
+        </li>
+        <li>
+          Snipe the crocs photo by u/Ryparian on{" "}
+          <a
+            target="_blank"
+            href="https://www.reddit.com/r/FindTheSniper/top/?t=year"
+            rel="noopener noreferrer"
+          >
+            Find the sniper
+          </a>
+        </li>
+        <li>
+          Snipe the panda photo from{" "}
+          <a
+            target="_blank"
+            href="https://brightside.me/articles/test-find-the-hidden-objects-in-these-15-images-797240/"
+            rel="noopener noreferrer"
+          >
+            Brightside
+          </a>
+        </li>
+      </ul>
+    </>
   );
 };
 
---
+++ src/attributions/Attributions.test.jsx
@@ -0,0 +1,36 @@
+import { describe, expect, it } from "vitest";
+import { Attributions } from "./Attributions";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { render, screen, within } from "@testing-library/react";
+
+describe("Attributions component", () => {
+  const mockRoutes = [
+    {
+      path: "/",
+      element: <p>Index route</p>,
+    },
+    {
+      path: "/attributions",
+      element: <Attributions />,
+    },
+  ];
+
+  const router = createMemoryRouter(mockRoutes, {
+    initialEntries: ["/attributions"],
+  });
+
+  it("Renders attributions page correctly", () => {
+    render(<RouterProvider router={router} />);
+
+    expect(
+      screen.getByRole("heading", { name: /Attributions/i }),
+    ).toBeInTheDocument();
+    expect(screen.getByTestId("attributions-para")).toBeInTheDocument();
+
+    const attributionsList = screen.getByRole("list", {
+      name: /^Attributions list$/i,
+    });
+    expect(attributionsList).toBeInTheDocument();
+    expect(within(attributionsList)).toHaveLength(3);
+  });
+});
