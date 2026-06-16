2737f977f43dffccf11d44b5abd56c43cd298d03
Vinson <vinsonhe2@gmail.com>
2026-06-16 15:04:07 -0700

    feat(deploy): set up project for deployment
    - Install gh-pages package to deploy React app to Github
    - Add base url in vite.config.js to prevent unexpected errors
    - Use hashRouter instead of browserRouter to make Github pages links
      work correctly

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
